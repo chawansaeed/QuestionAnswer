96e7d2ccd53530707672b225262f6f60e2aaf7f6
Chawan Saeed <chawan.saeed02@gmail.com>
2021-09-21 20:11:37 +0300

    Initial Commit

---
+++ IQTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IQTestApp
+//
+//  Created by Chawan Saeed on 9/21/21.
+//  Copyright © 2021 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
